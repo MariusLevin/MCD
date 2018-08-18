c55c300dd4a8c724762a57c691304868be55ed7e
Taylor Cox <Coxt3@myumanitoba.ca>
2018-08-18 13:15:29 -0500

    Complete Spotipy test

---
+++ api_test.py
@@ -0,0 +1,17 @@
+import spotipy
+from sys import argv
+
+from spotipy.oauth2 import SpotifyClientCredentials
+
+client_creds_mgr = SpotifyClientCredentials(client_id=argv[1], client_secret=argv[2])
+
+spotify = spotipy.Spotify(client_credentials_manager=client_creds_mgr)
+results = spotify.search(q='artist:weezer', type='artist')
+
+data = results['artists']['items']
+
+for d in data:
+  print d['name']
+
+# client: e394642c72f1484fbe9661aecede6d6c
+# secret: fbc0bc0391224c1c8360d427c913eb4e
